7dfac5c1ee0c68e15f4d91fc2352f5bc5e16b06c
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2025-01-20 13:03:28 +0100

    chore: less strict peer deps

--- lib/cosmos-db/index.ts
+++ lib/cosmos-db/index.ts
@@ -1,5 +1,6 @@
 export * from './cosmos-db.constants';
-export * from './cosmos-db.module';
-export * from './cosmos-db.interface';
 export * from './cosmos-db.decorators';
+export * from './cosmos-db.interface';
+export * from './cosmos-db.module';
 export * from './cosmos-db.providers';
+export { getConnectionToken, getModelToken } from './cosmos-db.utils';
